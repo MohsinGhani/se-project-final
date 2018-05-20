bd97cd22f21c5ad413575a29228ce171c4d092f9
mohsinghani <mohsinghani.777@gmail.com>
2018-05-20 07:47:46 +0500

    update db

--- university.sql
+++ university.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 18, 2018 at 01:59 PM
+-- Generation Time: May 20, 2018 at 02:47 AM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -25,6 +25,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `available_course`
+--
+
+DROP TABLE IF EXISTS `available_course`;
+CREATE TABLE IF NOT EXISTS `available_course` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(30) NOT NULL,
+  `section` varchar(5) NOT NULL,
+  `days` varchar(30) NOT NULL,
+  `time_slot` varchar(30) NOT NULL,
+  `t_email` varchar(30) NOT NULL,
+  `t_name` varchar(30) NOT NULL,
+  `course_code` varchar(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `available_course`
+--
+
+INSERT INTO `available_course` (`id`, `course_name`, `section`, `days`, `time_slot`, `t_email`, `t_name`, `course_code`) VALUES
+(1, 'w', 'A', 'Mon - Wed - Fri', '1 to 2', 'mohsin@gmail.com', 'Mohsin Ghani', 'w'),
+(2, 'w', 'A', 'Mon - Wed - Fri', '2 to 3', 'mohsin@gmail.com', 'Mohsin Ghani', 'w'),
+(3, 'w', 'A', 'Tue - Thu - Sat', '1 to 2', 'mohsin@gmail.com', 'Mohsin Ghani', 'w');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `offered_course`
 --
 
@@ -41,7 +69,8 @@ CREATE TABLE IF NOT EXISTS `offered_course` (
 --
 
 INSERT INTO `offered_course` (`code`, `name`, `program`) VALUES
-('w', 'w', 'BS-CS');
+('w', 'w', 'BS-CS'),
+('555', 'Software', 'BS-CS');
 
 -- --------------------------------------------------------
 
@@ -70,6 +99,27 @@ INSERT INTO `student` (`name`, `email`, `dob`, `program`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `student_course`
+--
+
+DROP TABLE IF EXISTS `student_course`;
+CREATE TABLE IF NOT EXISTS `student_course` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `s_email` varchar(30) NOT NULL,
+  `course_id` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student_course`
+--
+
+INSERT INTO `student_course` (`id`, `s_email`, `course_id`) VALUES
+(9, 'jimovera@emailsy.info', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `teacher`
 --
 
@@ -86,7 +136,8 @@ CREATE TABLE IF NOT EXISTS `teacher` (
 --
 
 INSERT INTO `teacher` (`name`, `email`, `password`) VALUES
-('sp16-bs-0065', 'jimovera@emailsy.info', 1);
+('sp16-bs-0065', 'jimovera@emailsy.info', 1),
+('Mohsin Ghani', 'mohsin@gmail.com', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
